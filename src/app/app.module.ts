adb654c740bb0c1b86417d09dbf357a0f6727b11
Sohamk0301 <100216902+Sohamk0301@users.noreply.github.com>
2023-03-28 15:17:20 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {ModuleoneModule} from './moduleone/moduleone.module';
+import { ModuleoneModule } from './moduleone/moduleone.module';
 import { ComponentoneComponent } from './moduleone/componentone/componentone.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ModuleoneModule],
-  declarations: [ AppComponent, HelloComponent, ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ModuleoneModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
